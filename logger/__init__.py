20faedfbf082b8bd717dd13b01c79dd97347df0a
Andrew D. Yates <andrewyates.name@gmail.com>
2012-04-08 00:35:41 -0500

    environment handling

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+import os
 import stderr_logger
 
-# set context sensitive logger
-if "SERVER" in globals() and SERVER:
+# Set context sensitive logger.
+if 'ENV' in os.environ and os.environ['ENV'] == "SERVER":
     Log = server_logger.Log
 else:
     Log = stderr_logger.Log
--- logger/server_logger.py
+++ logger/server_logger.py
@@ -8,14 +8,18 @@ Python v2.7.2 documentation: Python HOWTOs.
 [http://docs.python.org/howto/logging-cookbook.html]
 """
 import logging
+import os
 
+# Get log directory from environment variable.
+if 'LOGFILE' not in os.environ:
+  raise Exception, "Set os.environ variable 'LOGFILE' to full path to log file."
+else:
+  log_file = os.environ['LOGFILE']
 
-Log = logging.getLogger("server_logger")
+Log = logging.getLogger(log_file)
 Log.setLevel(logging.DEBUG)
 
-# StreamHandler() uses sys.stderr by default.
-# http://docs.python.org/library/logging.handlers.html
-LOGFILE = 'log' # XXX: THIS NEEDS TO BE SET DYNAMICALLY
+# See: http://docs.python.org/library/logging.handlers.html
 fh = logging.FileHandler(LOGFILE)
 fh.setLevel(logging.DEBUG)
 f = logging.Formatter("%(levelname)s %(asctime)s %(module)s.%(funcName)s %(lineno)d %(message)s")
