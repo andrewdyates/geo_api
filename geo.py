081da8d1e8cfd69112149b125765ec9265b8a5cb
Andrew D. Yates <andrewyates.name@gmail.com>
2012-10-02 22:06:15 -0400

    added support for methylation platform detection

--- geo.py
+++ geo.py
@@ -771,7 +771,12 @@ class GPL(object):
       'META': set(['SNP', 'nucleotide']),
       'SNP_ID': set(['snp', 'id', 'rs', 'snpid', 'ncbi']),
       'CHROMOSOME': set(['chromosome', 'chrom', 'chr', 'ch']),
-      'LOCATION': set(['mapinfo', 'map', 'info', 'loci', 'locus', 'loc', 'location', 'pos', 'position'])}
+      'LOCATION': set(['mapinfo', 'map', 'info', 'loci', 'locus', 'loc', 'location', 'pos', 'position'])},
+    'METHYLATION': {
+      'META': set(['humanmethylation', 'cpg', 'methylation']),
+      'GENE_SYMBOL': set(['gene', 'symbol', 'sym', 'genesym', 'genesymbol']),
+      'NAME': set(['name'])
+      }
     }
   # Preferred order of gene row identification symbol
   EQTL_GENE_NAME_LIST = [
